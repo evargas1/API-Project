3dc6277a32cfacf20732e95e0d534d965f2d4f90
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-01-14 14:54:55 -0600

    added code to project to find other information for the cities ---- adding cities next

--- script.js
+++ script.js
@@ -10,30 +10,26 @@ function(data) {
         var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
         var temp = Math.floor(data.main.temp);
         var weather = data.weather[0].description;
+        var feels = data.main.feels_like;
+        var humidity = data.main.humidity;
+        var wind = data.wind.speed;
+        var  cityName = data.name;
+        var cloudy = data.clouds.all;
 
         $(".icon").attr("src", icon);
         $(".weather").append(weather);
         $(".temp").append(temp);
+        $(".feels").append(feels);
+        $(".humidity").append(humidity);
+        $(".wind").append(wind);
+        $(".cityB").append(cityName);
+        $(".cloudy-percentage").append(cloudy);
         // if you wanted to add this before you would use the function .prepend
+
+        console.log(data);
         
 
     }
 );
 
 
-// exmaple from my book that I need to understand.
-
-$function() {
-    function loadContent(url){
-        $('#content').load(url + '#container').hide().fadeIn('slow');
-
-    }
-
-    $('nav a').on('click', function(e) {
-        e. preventDefault();
-        var href = this.href;
-        var $this = $(this);
-        $('a').removeClass('current');
-        $this 
-    })
-}
